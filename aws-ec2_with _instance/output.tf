d0d345a7c11bfc5df95ce256bc2b58a86bd14858
Shweta _d7403 <159001390+shweta-7403@users.noreply.github.com>
2025-01-23 23:49:03 +0530

    Delete aws-ec2_with _instance directory

--- aws-ec2_with _instance/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_instance_public_ip" {
-  value = aws_instance.webconsole.public_ip
-}
--- aws-ec2_with _instance/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "region" {
-    description = "value of region"
-    default = "us-east-1"
-    type = string
-}
